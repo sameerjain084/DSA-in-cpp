c9b72878480aafe5f4dd396463e639abd28c275d
aaaatechnologiesllc <153011602+aaaatechnologiesllc@users.noreply.github.com>
2026-04-12 16:18:19 +0530

    Added basic string program

---
+++ Strings/basic.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    /*string str ="apna college";
+    cout<<str<<endl;
+   
+    str = "hello";
+    cout<<str<<endl;*/
+
+    /*string str1 = "sameer";
+    string str2 = "jain";
+    cout<<(str1+str2)<<endl;*/
+
+    /*string str;
+    getline(cin,str);
+
+    cout<<str<<endl;*/
+    /*string str = "apna college";
+
+    for(char ch:str) {
+        cout<< ch<< " ";
+    }
+    cout<<endl;*/
+    /*string str = "apna college";
+    reverse(str.begin(), str.end());
+    cout<<str<<endl;*/
+
+
+    
+
+    return 0;
+
+
+}
