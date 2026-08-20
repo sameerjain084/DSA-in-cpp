77826ad91dc536e540fcbb7d81bfdf7523138e1a
aaaatechnologiesllc <153011602+aaaatechnologiesllc@users.noreply.github.com>
2026-08-20 10:54:09 +0530

    new file added

---
+++ Queue/queueusingll.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class Queue {
+    Node* head;
+    Node* tail;
+public:
+    Queue() {
+        head = tail = NULL;
+    }
+
+    void push(int data){// insert data at tail
+        Node* newNode = new Node(data);
+        if(empty()){
+            head = tail =newNode;
+        } else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop(){
+        if(empty()){
+            cout<<"LL is empty\n";
+            return ;
+        } else{
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    int front() {
+        if(empty()){
+            cout<<"LL is Empty\n";
+            return -1;
+        }
+        return head->data;
+
+    }
+
+    bool empty(){
+        return head ==NULL;
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    while(!q.empty()){
+        cout<< q.front()<< " ";
+        q.pop();
+    }
+
+    cout<<endl;
+
+    return 0;
+}
